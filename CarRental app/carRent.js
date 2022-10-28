fdf4f0098910d89eed52c43a100399adc301bce0
Jadyn-Jenkins <Noblejadynjenkins@gmail.com>
2022-10-28 15:46:19 -0400

    create js object reference variables

---
+++ CarRental app/carRent.js
@@ -0,0 +1,22 @@
+const dateIn = document.querySelector('input#pickupDate');
+const daysIn = document.querySelector('input#daysRented');
+
+const elecCheckB = document.querySelector('input#elecIn');
+const gpsCheckB = document.querySelector('input#gpsIn');
+const roadsideCheckB = document.querySelector('input#roadsideIn');
+
+const under25Radio = document.querySelector('input#no');
+const over25Radio = document.querySelector('input#yes');
+
+const calcBtn = document.querySelector('input#estimateTotalCost');
+
+const rentCostOut = document.querySelector('output#rentCost');
+const optionOut = document.querySelector('output#optionCarge');
+const ageOut = document.querySelector('output#ageCharge');
+const totalOut = document.querySelector('output#totalCost');
+
+addEventListener.calcBtn('click', )
+
+function findAndDisplayReceipt(params) {
+    
+}
